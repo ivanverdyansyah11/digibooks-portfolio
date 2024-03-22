6ced15121b81e489931e2e9f854e967d7ffcbcad
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-22 23:05:18 +0800

    Progress: Add New Component Footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,74 @@
+{{-- FOOTER --}}
+<footer class="footer" id="footer">
+    <div class="container">
+        <div class="row" style="margin-bottom: 100px">
+            <div class="col-6 col-md-3 mb-5 mb-md-0">
+                <a class="footer-brand d-inline-block" href="#home" style="margin-bottom: 18px;">
+                    <img src="{{ asset('assets/img/logo/logo-brand.svg') }}" class="img-fluid w-100" alt="Logo Brand">
+                </a>
+                <p class="paragraph-small" style="margin-bottom: 30px;">Welcome to Chroma, the ultimate
+                    destination for digital art enthusiasts and creators.</p>
+                <div class="wrapper-sosmed d-flex gap-2">
+                    <a href="https://id-id.facebook.com/" target="_blank"
+                        class="sosmed-icon d-flex justify-content-center align-items-center">
+                        <img src="{{ asset('assets/img/icon/facebook-icon.svg') }}" class="img-fluid"
+                            alt="Facebook Icon" width="16">
+                    </a>
+                    <a href="https://www.whatsapp.com/" target="_blank"
+                        class="sosmed-icon d-flex justify-content-center align-items-center">
+                        <img src="{{ asset('assets/img/icon/whatsapp-icon.svg') }}" class="img-fluid"
+                            alt="Whatsapp Icon" width="16">
+                    </a>
+                    <a href="https://twitter.com/?lang=id" target="_blank"
+                        class="sosmed-icon d-flex justify-content-center align-items-center">
+                        <img src="{{ asset('assets/img/icon/twitter-icon.svg') }}" class="img-fluid" alt="Twitter Icon"
+                            width="16">
+                    </a>
+                </div>
+            </div>
+
+            <div class="offset-lg-2 offset-xl-1 col-6 col-md-3 col-lg-2 mb-5 mb-md-0">
+                <h6 style="margin-bottom: 20px">Quick Links</h6>
+                <div class="wrapper d-flex flex-column gap-3">
+                    <a href="#home" class="paragraph-small">Home</a>
+                    <a href="#about" class="paragraph-small">About Us</a>
+                    <a href="#collection" class="paragraph-small">Collection Book</a>
+                    <a href="#team" class="paragraph-small">Our Team</a>
+                </div>
+            </div>
+            <div class="col-lg-2 d-none d-xl-inline-block">
+                <h6 style="margin-bottom: 20px">Our Featured</h6>
+                <div class="wrapper d-flex flex-column gap-3">
+                    <a href="#featured" class="paragraph-small">The Digital Revolution</a>
+                    <a href="#featured" class="paragraph-small">The Power of Imagination</a>
+                    <a href="#featured" class="paragraph-small">Literature's Hidden Meanings</a>
+                    <a href="#featured" class="paragraph-small">The Adventures of Digibot</a>
+                </div>
+            </div>
+            <div class="col-6 col-md-3 col-xl-2">
+                <h6 style="margin-bottom: 20px">Our Adventage</h6>
+                <div class="wrapper d-flex flex-column gap-3">
+                    <a href="#adventage" class="paragraph-small">Unlimited Access</a>
+                    <a href="#adventage" class="paragraph-small">Anytime, Anywhere Reading</a>
+                    <a href="#adventage" class="paragraph-small">Interactive and Engaging</a>
+                    <a href="#adventage" class="paragraph-small">Discoverability</a>
+                </div>
+            </div>
+            <div class="col-6 col-md-3 col-lg-2">
+                <h6 style="margin-bottom: 20px">Help & guide</h6>
+                <div class="wrapper d-flex flex-column gap-3">
+                    <a href="#" class="paragraph-small">Terms & Conditions</a>
+                    <a href="#" class="paragraph-small">Privacy Policy</a>
+                    <a href="#" class="paragraph-small">Contact Us</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 copyright text-center">
+                <p>Copyright © 2023 Digibooks. All Right Reserved.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+{{-- END FOOTER --}}
